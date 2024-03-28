7af416ba761203d7d7819bcb454b8259d5f73de2
toni-santos <a.oliveirasantos122@gmail.com>
2024-03-28 14:45:05 +0000

    implement histogram equalization and gamma adjustment

--- __init__.py
+++ __init__.py
@@ -2,42 +2,35 @@ import cv2, os
 import numpy as np
 import matplotlib.pyplot as plt
 
-def dog(img,k1,s1,k2,s2):
-    b1 = cv2.GaussianBlur(img,(k1, k1), s1)
-    b2 = cv2.GaussianBlur(img,(k2, k2), s2)
-    return b1 - b2
-
-img_folder = os.listdir('./easy_samples/')
+img_folder = os.listdir('./samples/')
 for path in img_folder:
     img_path = f"samples/{path}"
     total_pieces = 0
+    kernel = np.ones((5, 5), np.uint8) 
+    kernel2 = np.ones((13, 13), np.uint8) 
 
     img = cv2.imread(img_path)
-    # img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+    # img = cv2.resize(img, (0, 0), fx=0.70, fy=0.70)
     im = img.copy()
+    MIN_AREA = img.shape[0] * img.shape[1] * 0.001
+
+    img = cv2.fastNlMeansDenoisingColored(img,None,10,7,21)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.equalizeHist(img)
+    gamma = 2
+    invGamma = 1/gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+                    for i in np.arange(0, 256)]).astype("uint8")
+    img = cv2.LUT(img, table, img)
 
-    MIN_AREA = img.shape[0] * img.shape[1] * 0.01
+    img = cv2.bilateralFilter(img,9,img.shape[0]//25,img.shape[0]//25)
 
-    kernel = np.ones((5, 5), np.uint8) 
-
-    # TODO: find better contrast algorithm
-    # for y in range(img.shape[0]):
-    #     for x in range(img.shape[1]):
-    #         for c in range(img.shape[2]):
-    #             img[y,x,c] = np.clip(alpha*img[y,x,c] + beta, 0, 255)
-
-    blur_img = cv2.GaussianBlur(img, (5, 5), 0)
-    # img = cv2.divide(img, blur_img, scale=196)
-    # img = cv2.bilateralFilter(img, 9, 50, 50)
-
-    # TODO: maybe don't hardcode the values?
     img_canny = cv2.Canny(img, 50, 100)
-    # img_canny = dog(img,7,7,15,15)
 
-    img_canny = cv2.morphologyEx(img_canny, cv2.MORPH_CLOSE, kernel)
+    img_canny = cv2.morphologyEx(img_canny, cv2.MORPH_DILATE, kernel, iterations=1)
+    img_canny = cv2.morphologyEx(img_canny, cv2.MORPH_CLOSE, kernel, iterations=2)
 
-    thresh = cv2.threshold(img_canny, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    thresh = cv2.threshold(img_canny, 100, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
     contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if len(contours) == 2 else contours[1]
@@ -62,9 +55,9 @@ for path in img_folder:
 
         if draw:
             total_pieces += 1
-            cv2.rectangle(im, (x, y), (x+w, y+h), (0, 0, 255), 2)
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
     print(f"Total pieces: {total_pieces}")
 
-    cv2.imwrite(f'easy_output/canny_{path}', img_canny)
-    cv2.imwrite(f'easy_output/{path}', im)
+    cv2.imwrite(f'output/canny_{path}', img_canny)
+    cv2.imwrite(f'output/{path}', img)
