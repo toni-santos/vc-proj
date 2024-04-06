33b7259fd561a27a11853cb50e3aac61f2c94f89
toni-santos <a.oliveirasantos122@gmail.com>
2024-04-06 17:47:24 +0100

    finished grabcut and implemented better color detection

--- __init__.py
+++ __init__.py
@@ -5,6 +5,37 @@ import json
 import argparse
 import time
 
+# color dictionary
+COLORDICT= {
+    # "Red": [(0, 60)],
+    "Yellow": [(20, 40)],
+    "Green": [(41, 70)],
+    "Cyan": [(71, 100)],
+    "Blue": [(101, 130)],
+    "Magenta": [(131, 160)]
+}
+
+def getColorName(color):
+
+    color = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
+
+    h, s, v = cv2.split(color)   # H = 0-179, S = 0-255, V = 0-255
+
+    if v < 30:
+        return "Black"
+    
+    if s < 30:
+        if v > 230:
+            return "White"
+        return "Gray"
+    
+    for color_name, ranges in COLORDICT.items():
+        for start, end in ranges:
+            if start <= h <= end:
+                return color_name
+
+    return "Red"
+
 # INFO: connectedComponents was previously implemented and part of the solution however it was not used in the final version of the code, keeping it as an artifact 
 def connectedComponents(img, MIN_AREA, unprocessed = False):
     output = img.copy()
@@ -40,6 +71,16 @@ def connectedComponents(img, MIN_AREA, unprocessed = False):
 
     return objs, largest_area
 
+def inCorner(img, rect):
+    x, y, w, h = rect
+    return (x == 0 and y == 0) or (x + w == img.shape[1] and y == 0) or (y + h == img.shape[0] and x == 0) or (x + w == img.shape[1] and y + h == img.shape[0])
+
+def inContour(target, comp):
+    x, y, w, h = target
+    x1, y1, w1, h1 = comp
+
+    return x >= x1 and y >= y1 and x+w <= x1+w1 and y+h <= y1+h1
+
 def overlapOverThreshold(rect1, rect2, area_threshold):
     # Two rects overlap by more than 60% of the area of either rectangle?
     x1_overlap = max(0, min(rect1[2], rect2[2]) - max(rect1[0], rect2[0]))
@@ -74,9 +115,7 @@ def overlapUnion(rect1, rect2, threshold):
     # x y w h
     return [x1, y1, x2-x1, y2-y1]
 
-def getColor(img):
-    
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+def getColorBGR(img):
     
     height, width, channels = img.shape[:3]
 
@@ -103,13 +142,15 @@ def inColorThreshold(colors, color, threshold):
             return True
     return False
 
-def preProcessing(img):
+def preProcessing(img, simple = False):
     print(f"Preprocessing...")
     start_time = time.time()
 
     # Denoising
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.fastNlMeansDenoising(img,None,23,7,21)
+
+    if not simple:
+        img = cv2.fastNlMeansDenoising(img,None,23,7,21)
     
     # Histogram equalization
     img = cv2.equalizeHist(img)
@@ -151,16 +192,21 @@ def canny(img, canny_lower, canny_upper, threshold, maxval):
     print(f"Canny edge detection done in {time.time() - start_time} seconds")
     return contours, img_canny
 
-def findPieces(contours, img, img_og, MIN_AREA, gc = False):
+def findPieces(contours, img, img_og, MIN_AREA, path):
     print("Finding pieces...")
     start_time = time.time()
     copy_og = img_og.copy()
+
     objs = []
     colors = []
     overlap = True
     draw = True
     MAX_DIST = 20 # TODO: tweak this value
     rects = contours
+
+    for rect in rects:
+        if inCorner(img, rect):
+            rects.remove(rect)
 
     while overlap:
         overlap = False
@@ -176,27 +222,15 @@ def findPieces(contours, img, img_og, MIN_AREA, gc = False):
             if area < MIN_AREA:
                 continue
 
-            if ((x == 0 and y == 0) or
-            (x + w == img.shape[1] and y == 0) or
-            (y + h == img.shape[0] and x == 0) or
-            (x + w == img.shape[1] and y + h == img.shape[0])):
-                continue
-
             for n_idx, n_rect in enumerate(rects_copy):
                 x1,y1,w1,h1 = n_rect
                 n_area = w1 * h1
 
                 if n_area < MIN_AREA or n_idx == idx:
                     continue
-
-                if ((x1 == 0 and y1 == 0) or
-                (x1 + w1 == img.shape[1] and y1 == 0) or
-                (y1 + h1 == img.shape[0] and x1 == 0) or
-                (x1 + w1 == img.shape[1] and y1 + h1 == img.shape[0])):
-                    continue
                 
                 # Check if the current contour is inside another contour
-                if (x >= x1 and y >= y1 and x+w >= x1+w1 and y+h >= y1+h1):
+                if inContour(rect, n_rect):
                     draw = False
                     break
 
@@ -205,26 +239,24 @@ def findPieces(contours, img, img_og, MIN_AREA, gc = False):
                     overlap = True
                     x, y, w, h = overlapUnion([x, y, x+w, y+h], [x1, y1, x1+w1, y1+h1], MAX_DIST)
             
-            
-            area = w * h
-            if area < MIN_AREA:
-                # print("Area too small")
-                continue
-
-            area = w * h
-            if area < MIN_AREA:
-                # print("Area too small")
-                continue
             if draw:
                 rects.append([x, y, w, h])
 
         rects = [list(i) for i in set(tuple(i) for i in rects)]
 
+    test = img_og.copy()
+    for rect in rects:
+        x, y, w, h = rect
+        cv2.rectangle(test, (x, y), (x + w, y + h), (0, 255, 0), 1)
+    cv2.imwrite(f'json_output/rects_{path}', test)
+
     for rect in rects:
         x, y, w, h = rect
 
         zone = img_og[y:y+h, x:x+w]
+        ZONE_MIN_AREA = zone.shape[1] * zone.shape[0] * 0.01
 
+        # GrabCut
         mask = np.zeros(zone.shape[:2],np.uint8)
         rect = (1,1,zone.shape[1],zone.shape[0])
         bgdModel = np.zeros((1,65),np.float64)
@@ -234,29 +266,58 @@ def findPieces(contours, img, img_og, MIN_AREA, gc = False):
         mask = np.where((mask==2)|(mask==0),0,1).astype('uint8')
         zone = zone*mask[:,:,np.newaxis]
 
-        zone = preProcessing(zone)
+        # simple preprocess -> canny -> thresh -> close -> erode -> find contours
+        zone = preProcessing(zone, simple=True)
+
+        zone = cv2.Canny(zone, 0, 0)
 
         thresh = cv2.threshold(zone, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-        # TODO: do morphological operations here
+        kernel = np.ones((5, 5), np.uint8)
+        erode_kernel = np.ones((9, 9), np.uint8)
+        thresh = cv2.morphologyEx(thresh, cv2.MORPH_DILATE, kernel, iterations=2)
+        thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+        thresh = cv2.morphologyEx(thresh, cv2.MORPH_ERODE, erode_kernel, iterations=3)
 
-        # TODO: fix this so that it uses the contours from the grabcut
-        cv2.rectangle(copy_og, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        contours_ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours_ = contours_[0] if len(contours_) == 2 else contours_[1]
+        contours_ = [[x,y,w,h] for x, y, w, h in [cv2.boundingRect(c) for c in contours_]]
 
-        objs.append({
-            "xmin": x,
-            "ymin": y,
-            "xmax": x + w,
-            "ymax": y + h
-        })
+        for idx, c in enumerate(contours_):
+            x_, y_, w_, h_ = c
+            area_ = w_ * h_
+            skip = False
 
-        # Calculate the main color of the piece
-        cropped = img_og[y:y+h, x:x+w]
-        piece_color = tuple(getColor(cropped))
-        if not inColorThreshold(colors, piece_color,30):
-            colors.append(piece_color)        
-    
-    print(f"Found {len(objs)} pieces and {len(colors)} colors in {time.time() - start_time} seconds")
+            if area < ZONE_MIN_AREA:
+                continue
+
+            x_ = x + x_ 
+            y_ = y + y_
+
+            cv2.rectangle(copy_og, (x_, y_), (x_ + w_, y_ + h_), (0, 0, 255), 1)
+
+            objs.append({
+                "xmin": x_,
+                "ymin": y_,
+                "xmax": x_ + w_,
+                "ymax": y_ + h_
+            })
+
+            cropped = img_og[y_:y_+h_, x_:x_+w_]
+            piece_color = list(getColorBGR(cropped))
+            split = img_og[0:1,0:1]
+            split = np.array([[piece_color]])
+            # cv2.namedWindow('color', cv2.WINDOW_NORMAL)
+            # cv2.imshow('color', split)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+            print(split)
+            color = getColorName(split)
+
+            if color not in colors:
+                colors.append(color)    
+
+    print(f"Found {len(objs)} pieces and {len(colors)} ({colors}) colors in {time.time() - start_time} seconds")
 
     return colors, objs, copy_og
 
@@ -270,12 +331,11 @@ def run(path):
     canny_upper = 70
     threshold = 0
     maxval = 255
-    MAX_PIECES = 30
 
     # Read image
     img = cv2.imread(img_path)
-    # img = cv2.resize(img, (0, 0), fx=0.70, fy=0.70)
     img_og = img.copy()
+
     IMG_AREA = img.shape[0] * img.shape[1]
     MAX_AREA = IMG_AREA * 0.5
     MIN_AREA = IMG_AREA * 0.004
@@ -283,45 +343,10 @@ def run(path):
     img = preProcessing(img)
 
     contours, img_canny = canny(img, canny_lower, canny_upper, threshold, maxval)
-    cv2.imwrite(f'json_output/canny_{path}', img_canny)
+    # cv2.imwrite(f'json_output/canny_{path}', img_canny)
 
-    colors, objs, img_pieces = findPieces(contours, img, img_og, MIN_AREA)
+    colors, objs, img_pieces = findPieces(contours, img, img_og, MIN_AREA, path)
     cv2.imwrite(f'json_output/{path}', img_pieces)
-
-    if len(objs) > MAX_PIECES:
-        print("Found too many pieces, running GrabCut...")
-        start_time = time.time()
-        img = img_og.copy()
-        mask = np.zeros(img.shape[:2],np.uint8)
-        rect = (1,1,img.shape[1],img.shape[0])
-        bgdModel = np.zeros((1,65),np.float64)
-        fgdModel = np.zeros((1,65),np.float64)
-
-        mask, bgdModel, fgdModel = cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
-        mask = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-        img = img*mask[:,:,np.newaxis]
-        cv2.imwrite(f"json_output/grabcut_{path}", img)
-        print(f"GrabCut done in {time.time() - start_time} seconds")
-
-        img = preProcessing(img)
-
-        contours, img_canny = canny(img, canny_lower, canny_upper, threshold, maxval)
-        cv2.imwrite(f'json_output/canny_{path}', img_canny)
-
-        new_colors, new_objs, img_pieces = findPieces(contours, img, img_og, MIN_AREA, gc=True)
-
-        if len(new_objs) < len(objs):
-            print("GrabCut pieces result is better")
-            objs = new_objs
-            cv2.imwrite(f'json_output/{path}', img_pieces)
-        else:
-            print("Original pieces result is better")
-
-        if len(new_colors) > len(colors):
-            print("GrabCut colors result is better")
-            colors = new_colors
-        else:
-            print("Original colors result is better")
 
     return objs, len(colors)
 
